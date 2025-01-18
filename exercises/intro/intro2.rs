184d8f1fbbfb5472cf01c1f81e42b77016e60505
dyingforge <1289470395.@qq.com>
2025-01-18 18:09:50 +0800

    update

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -5,8 +5,8 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    const str1:&str = "world";
+    println!("Hello {}!",str1);
 }
